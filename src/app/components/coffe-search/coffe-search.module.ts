c2969e8d28911450fdfa54ff650c5f9e00fe84a2
Bruno Brito <bruno@brittus.com>
2024-01-10 18:57:32 -0300

    Formulario e novos componentes Material

--- src/app/components/coffe-search/coffe-search.module.ts
+++ src/app/components/coffe-search/coffe-search.module.ts
@@ -1,7 +1,11 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoffeSearchComponent } from './coffe-search.component';
 import {MatCardModule} from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatStepperModule} from '@angular/material/stepper';
 
 @NgModule({
   declarations: [
@@ -9,7 +13,11 @@ import {MatCardModule} from '@angular/material/card';
   ],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    MatStepperModule,
+    MatRadioModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   exports: [
     CoffeSearchComponent
